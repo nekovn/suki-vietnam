797659c78f59ca6557a1001dc8a06a3a547b7d07
Rim <rim281195@gmail.com>
2021-07-08 16:30:25 +0900

    update site map

--- frontend/oonavi/vue.config.js
+++ frontend/oonavi/vue.config.js
@@ -12,4 +12,14 @@ module.exports = {
             errors: false
         }
     },
+    pluginOptions: {
+        sitemap: {
+            urls: [
+                'http://www.suki-vietnam.com',
+                'http://www.suki-vietnam.com/about',
+                'http://www.suki-vietnam.com/copyright',
+                'http://www.suki-vietnam.com/inquiry',
+            ]
+        }
+    }
 }
