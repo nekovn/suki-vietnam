dd9716d4ca1cb063614e3a1bf198e578cf7259f5
Rim <77607294+nekovn@users.noreply.github.com>
2021-07-10 23:49:08 +0900

    Update about.vue

--- frontend/oonavi/src/pages/footer/about.vue
+++ frontend/oonavi/src/pages/footer/about.vue
@@ -19,12 +19,12 @@
 
           </div>
           <div class="text-2xl tracking-tight font-mono">
-            <p class="lg:text-2xl xl:text-2xl 2xl:text-2xl text-sm">
+            <p class="lg:text-2xl xl:text-2xl 2xl:text-2xl text-sm mb-1">
               当サイトは、日本国籍保有者が日本からベトナムに入国する場合、ベトナムの快速検索や、簡単なカテゴリから探すサービスとして、様々なコンテンツサービスを提供しています。
               日本人向けベトナムのニュースやベトナムビザやベトナム大使館や在ベトナム日本国大使館や天気など毎日欠かさず確認するのサイトです。それから、ベトナムの状況を常に正確に把握しご満足いただける最適で高品質なサービスをご提供しています。緊急時やトラブルの時すぐに探したい情報や
               相談・質問・比較などができるお役立ちサイトなど、インターネット上で便利に使えるサイトを厳選して集めています。
             </p>
-            <ol class="list-disc lg:text-xl xl:text-xl 2xl:text-xl text-xs ml-12 -mt-1">
+            <ol class="list-disc lg:text-xl xl:text-xl 2xl:text-xl text-xs ml-12">
               <li><span class="font-bold">本社所在地</span>：東京都豊島区西池袋5-19-20</li>
               <li><span class="font-bold">電話番号</span>：090 2259 2811</li>
               <li><span class="font-bold">メールアドレス</span>：sukivietnam19092811@gmail.com</li>
